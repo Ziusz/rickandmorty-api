c92eb503e018941a040b9ecc9bb05528e6046edc
Ziusz <podengo@tlen.pl>
2025-01-08 20:28:52 +0100

    Modify Main Vue file to show all characters in a table

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,3 +1,45 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const characters = ref([]);
+
+const fetchCharacters = async () => {
+    try {
+        const response = await axios.get('/api/characters');
+        characters.value = await response.data;
+    } catch (error) {
+        console.error('Occurred error: ', error);
+    }
+}
+
+onMounted(() => {
+    fetchCharacters();
+})
+</script>
+
 <template>
-    <h1>Test</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Location</th>
+                <th>Last episode</th>
+                <th>Species</th>
+                <th>Origin</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="character in characters">
+                <td>{{ character.id }}</td>
+                <td>{{ character.name }}</td>
+                <td>{{ character.status }}</td>
+                <td>{{ character.location }}</td>
+                <td>{{ character.last_episode }}</td>
+                <td>{{ character.species }}</td>
+                <td>{{ character.origin }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
