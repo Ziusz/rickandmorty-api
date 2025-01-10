dbeee5d211aaf854d5ca52b884f9339f66bd16a4
Ziusz <podengo@tlen.pl>
2025-01-10 11:50:15 +0100

    Add some Tailwind styles to beautify the table

--- resources/pages/app.vue
+++ resources/pages/app.vue
@@ -35,31 +35,34 @@ onMounted(() => {
 </script>
 
 <template>
-    <table>
-        <thead>
+    <div v-if="characters.length == 0" class="p-2">
+        <span>No data to display.</span>
+    </div>
+    <table class="w-full bg-white overflow-hidden" v-else>
+        <thead class="bg-blue-200">
             <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Status</th>
-                <th>Location</th>
-                <th>Last episode</th>
-                <th>Species</th>
-                <th>Origin</th>
-                <th>Created at</th>
-                <th>Updated at</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">ID</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Name</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Status</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Location</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Last episode</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Species</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Origin</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Created at</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Updated at</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="character in characters">
-                <td>{{ character.id }}</td>
-                <td>{{ character.name }}</td>
-                <td>{{ character.status }}</td>
-                <td>{{ character.location }}</td>
-                <td>{{ character.last_episode }}</td>
-                <td>{{ character.species }}</td>
-                <td>{{ character.origin }}</td>
-                <td>{{ formatDate(character.created_at) }}</td>
-                <td>{{ formatDate(character.updated_at) }}</td>
+            <tr v-for="character in characters" class="hover:bg-gray-50">
+                <td class="px-6 py-3 text-sm text-gray-900">{{ character.id }}</td>
+                <td class="px-6 py-3 text-m font-medium text-gray-900">{{ character.name }}</td>
+                <td class="px-6 py-3 text-sm text-gray-500">{{ character.status }}</td>
+                <td class="px-6 py-3 text-sm text-gray-500">{{ character.location }}</td>
+                <td class="px-6 py-3 text-sm text-gray-500">{{ character.last_episode }}</td>
+                <td class="px-6 py-3 text-sm text-gray-500">{{ character.species }}</td>
+                <td class="px-6 py-3 text-sm text-gray-500">{{ character.origin }}</td>
+                <td class="px-6 py-3 text-xs text-gray-500">{{ formatDate(character.created_at) }}</td>
+                <td class="px-6 py-3 text-xs text-gray-500">{{ formatDate(character.updated_at) }}</td>
             </tr>
         </tbody>
     </table>
