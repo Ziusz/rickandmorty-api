9c39b0404a94a6588fc8c34e20202188710767ea
Ziusz <podengo@tlen.pl>
2025-01-10 11:56:33 +0100

    Change the name of the page to home

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import mainApp from '../pages/app.vue';
+import mainApp from '../pages/home.vue';
 
 const app = createApp(mainApp);
 app.mount('#app');
