0e735ab51d96fbeb5dae81424c2354eafc2fe365
Ziusz <podengo@tlen.pl>
2025-01-12 18:28:53 +0100

    Modify structure of CharacterCreated to match front-end structure

--- app/Events/CharacterCreated.php
+++ app/Events/CharacterCreated.php
@@ -2,36 +2,44 @@
 
 namespace App\Events;
 
-use App\Contracts\CharacterServiceInterface;
+use App\Models\Character;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class CharacterCreated implements ShouldBroadcast
+class CharacterCreated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    public $character;
 
     /**
      * Create a new event instance.
      */
-    public function __construct(private readonly CharacterServiceInterface $characterService)
+    public function __construct(private readonly Character $character) 
     {
-        $this->character = $this->characterService->getCharacter($characterId);
+
     }
 
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
+     * @return Channel
      */
     public function broadcastOn(): Channel
     {
         return new Channel('characters');
+    }
+
+    /**
+     * Convert data to array.
+     *
+     * @return array
+     */
+    public function broadcastWith(): array
+    {
+        return $this->character->toArray();
     }
 }
