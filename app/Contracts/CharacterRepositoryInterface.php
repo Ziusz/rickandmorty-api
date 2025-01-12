4f9f69d107c4a3fc1332d5bf16a1f2186918be6b
Ziusz <podengo@tlen.pl>
2025-01-12 20:58:03 +0100

    Fix typo in CharacterRepositoryInterface

--- app/Contracts/CharacterRepositoryInterface.php
+++ app/Contracts/CharacterRepositoryInterface.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Collection;
 
 interface CharacterRepositoryInterface
 {
-    public function all(): Collection;
+    public function getAll(): Collection;
     public function create(array $data): Character;
 }
