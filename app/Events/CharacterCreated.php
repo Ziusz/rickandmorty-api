23c29955b8d38b4825f7d418f436596504478a12
Ziusz <podengo@tlen.pl>
2025-01-12 15:02:31 +0100

    Add Event for Creating New Character in DB

---
+++ app/Events/CharacterCreated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Contracts\CharacterServiceInterface;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CharacterCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $character;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(private readonly CharacterServiceInterface $characterService)
+    {
+        $this->character = $characterService->getCharacter($characterId);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): Channel
+    {
+        return new Channel('charactersUpdate');
+    }
+}
